9288ff6875e1457c14344ea82ff2fc5a39189e1e
Animesh Kumar <animesh99dev@gmail.com>
2021-11-23 04:24:12 +0530

    added optimizations

--- resources/script.js
+++ resources/script.js
@@ -26,3 +26,68 @@ else if (hrs >= 17 && hrs <= 24) greet = "Good Evening";
 document.querySelector("#greeting").innerHTML = greet;
 
 /////////// PAGE INDICATOR ///////////
+
+const heroEl = document.getElementById("hero");
+const aboutEl = document.getElementById("about");
+const workEl = document.getElementById("work");
+const collabEl = document.getElementById("collab");
+const nameEl = document.getElementById("current-name");
+let curName = "Animesh";
+
+const sectionOptions = {
+  rootMargin: "-200px 0px 0px 0px",
+};
+const sectionObserver = new IntersectionObserver(function (
+  entries,
+  sectionObserver
+) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      console.log(entry);
+      let cls = entry.target.id;
+      document.querySelector(`.${cls}`).classList.add("active");
+      nameEl.innerText = cls;
+    }
+    if (!entry.isIntersecting) {
+      let cls = entry.target.id;
+      document.querySelector(`.${cls}`).classList.remove("active");
+    }
+  });
+},
+sectionOptions);
+
+sectionObserver.observe(heroEl);
+sectionObserver.observe(aboutEl);
+sectionObserver.observe(workEl);
+sectionObserver.observe(collabEl);
+
+/////////// SMOOTH SCROLLING FUNCTIONALITY FOR SAFARI ///////////
+
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+
+    //Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
